2ea84a52d8a017b5c37380611c9633ea754c7bd1
Shayan Sadeghieh <shayansadeghieh@shayans-mbp.home>
2020-08-16 19:06:09 +0100

    moving along on the discriminator LSTM Cell

--- src/discriminator_model.py
+++ src/discriminator_model.py
@@ -3,35 +3,59 @@ from tensorflow.compat.v1.python.ops import rnn_cell_impl
 import pandas as pd
 import numpy as np
 
-class DiscriminatorLSTMCell(rnn_cell_impl.BasicLSTMCell):
+class DiscriminatorLSTMCell(rnn_cell_impl.RNNCell):
+    '''
+    Module implementing LSTM Cell. Child of tensorflow's
+    RNNCell.
+
+    The implementation is based on: http://arxiv.org/abs/1409.2329.
+    '''
     def __init__(self, 
                 num_units, 
-                num_inputs
+                num_inputs,
+                num_outputs,
                 activation,
                 reuse = False,
-                name,
-                dtype,
+                name = None,
+                dtype = None,
                 **kwargs
                 ):
-        '''
-        Args:
-        num_units: int, the number of units in the LSTM cell. 
-        num_inputs: int, the number of inputs into the LSTM cell.
-
-
-        '''
+    """Initialize the basic LSTM cell.
+    Args:
+      num_units: int, The number of units in the LSTM cell.
+      forget_bias: float, The bias added to forget gates (see above).
+        Must set to `0.0` manually when restoring from CudnnLSTM-trained
+        checkpoints.
+      state_is_tuple: If True, accepted and returned states are 2-tuples of
+        the `c_state` and `m_state`.  If False, they are concatenated
+        along the column axis.  The latter behavior will soon be deprecated.
+      activation: Activation function of the inner states.  Default: `tanh`.
+      reuse: (optional) Python boolean describing whether to reuse variables
+        in an existing scope.  If not `True`, and the existing scope already has
+        the given variables, an error is raised.
+      When restoring from CudnnLSTM-trained checkpoints, must use
+      CudnnCompatibleLSTMCell instead.
+    """
         super(DiscriminatorLSTMCell, self).__init__()
         self.num_units = num_units
         self.num_inputs = num_inputs
         self.activation = activation
+        self.num_outputs = num_outputs
         
-    
-    # if reuse == False:
-    #     self.w = self.BuildInputWeights()
-    
-    def BuildInputWeights(self):
-        pass 
+    self._num_units = num_units
+    self._activation = activation or math_ops.tanh
+    self._linear = None
 
+    #Required because it RNNCell is parent and 
+    # is an abstract class. However, could be unused. 
+    #DON'T KNOW YET!! 
+    @property
+    def state_size(self):
+        return self._num_units
+
+    @property
+    def output_size(self):
+        return self._num_units
 
 
     
