e156d1b3928711d0a1835dd3e960d8138754bdc2
shayansadeghieh <shayan.sadeghieh@gmail.com>
2020-09-14 11:02:28 +0100

    altered some stuff in disc

--- src/discriminator_model.py
+++ src/discriminator_model.py
@@ -8,7 +8,7 @@ import numpy as np
 #Import custom classes 
 from linear_class import _Linear
 
-class DiscriminatorLSTMCell(rnn_cell_impl.RNNCell):
+class LSTMCell(rnn_cell_impl.RNNCell):
     '''
     Module implementing LSTM Cell. Child of tensorflow's
     RNNCell.
@@ -32,53 +32,18 @@ class DiscriminatorLSTMCell(rnn_cell_impl.RNNCell):
         When restoring from CudnnLSTM-trained checkpoints, must use
         CudnnCompatibleLSTMCell instead.
         """
-        super(DiscriminatorLSTMCell, self).__init__()
-        if not state_is_tuple:
-                logging.warn("%s: Using a concatenated state is slower and will soon be "
-                        "deprecated.  Use state_is_tuple=True.", self)
-
+        super(LSTMCell, self).__init__()
         self._num_units = num_units
         self._forget_bias = forget_bias
         self._state_is_tuple = state_is_tuple
         self._activation = activation or math_ops.tanh
         self._linear = None
 
-    #Required because RNNCell is parent and 
-    # is an abstract class. However, could be unused. 
-    @property
-    def state_size(self):
-        return self._num_units
 
-    @property
-    def output_size(self):
-        return self._num_units 
-    
-    def call(self, inputs, state):
-        """Long short-term memory cell (LSTM).
-        Args:
-        inputs: `2-D` tensor with shape `[batch_size x input_size]`.
-        state: An `LSTMStateTuple` of state tensors, each shaped
-            `[batch_size x self.state_size]`, if `state_is_tuple` has been set to
-            `True`.  Otherwise, a `Tensor` shaped
-            `[batch_size x 2 * self.state_size]`.
-        Returns:
-        A pair containing the new hidden state, and the new state (either a
-            `LSTMStateTuple` or a concatenated state, depending on
-            `state_is_tuple`).
-        """
-        if self._state_is_tuple:
-            c, h = state
-        else:
-            c, h = array_ops.split(value=state, num_or_size_splits=2, axis=1)
-
-        sigmoid = math_ops.sigmoid
-
-        if self._linear is None:
-            self._linear = _Linear([inputs, h], 4 * self._num_units, True)
 
 
 if __name__ == "__main__":
-    discriminator_tester = DiscriminatorLSTMCell(10)
+    discriminator_tester = LSTMCell(10)
 
     print('ran discriminator successfully')
 
